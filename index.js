987c0e48144d66887816d12fb47ee124d41d5fef
He4eT <He4eT@oddsquat.org>
2023-12-25 14:55:18 +0100

    index.js: init

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-console.log(1234)
+const xml2js = require('xml2js')
+
+const channelsUrl = 'https://somafm.com/channels.xml'
+
+const download = (url) =>
+  fetch(url)
+  .then((response) => {
+    if (response.ok) {
+      return response.text()
+    } else {
+      throw new Error(`Failed to download XML. Status: ${response.status} ${response.statusText}`)
+    }
+  })
+
+download(channelsUrl)
+  .then(xml2js.parseStringPromise)
+  .then((json) => json.channels.channel)
+  .then((channels) => channels.map((channel) => ({
+    title: `${channel.title[0]} — ${channel.description[0]}`,
+    value: channel.highestpls[0]['_'],
+  })))
+  .then((x) => console.log(x))
+  .catch((error) => console.error('Error:', error.message))
